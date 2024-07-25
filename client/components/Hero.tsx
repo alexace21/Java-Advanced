a9bfb85b140ab6c14ef19a10c82d009b16d6e6d1
Alex Asenov <alex.ace7ov@gmail.com>
2024-07-25 13:58:25 +0300

    Finish Landing page outlook

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -29,7 +29,11 @@ const Hero = () => {
         </div>
         <div className='hero__image-container'>
             <div className='hero__image'>
-                
+                <Image src="/hero.png" alt='hero'
+                fill className='object-contain'
+                />
+
+                <div className='hero__image-overlay' />
             </div>
         </div>
     </div>
