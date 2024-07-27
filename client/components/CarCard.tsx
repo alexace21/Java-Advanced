18ce70d453c42cd6d106e92ddd2ac26c6b4f85c3
Alex Asenov <alex.ace7ov@gmail.com>
2024-07-27 16:00:14 +0300

    utilize CarCardProps

--- client/components/CarCard.tsx
+++ client/components/CarCard.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import React from 'react'
+import Image from "next/image";
+import { useState } from "react";
+import { CarCardProps } from "@/types";
+import CustomButton from "./CustomButton";
 
-const CarCard = () => {
+interface CarProps {
+    car: CarCardProps
+}
+
+const CarCard = ({ car }: CarProps) => {
   return (
     <div>CarCard</div>
   )
