937af3e930f34c4e9d2c9614147efe1e37b4ac3b
Alex Asenov <alex.ace7ov@gmail.com>
2024-07-29 16:51:20 +0300

    remove CarEntity from middle tier

--- src/main/java/softuni/defense/project/repositories/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package softuni.defense.project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import softuni.defense.project.model.entities.CarEntity;
-
-@Repository
-public interface CarRepository extends JpaRepository<CarEntity, Long> {
-}
