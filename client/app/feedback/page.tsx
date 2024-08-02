cd5b958e008be9a1d5338540e34f3309a76b7498
Alex Asenov <alex.ace7ov@gmail.com>
2024-08-02 19:39:42 +0300

    protect Feedback route from not authenticated user

--- client/app/feedback/page.tsx
+++ client/app/feedback/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import FeedbackForm from "@/components/FeedbackForm";
+import { useAuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const page = () => {
+
+  const {isAuthenticated, setIsAuthenticated} = useAuthContext();
+  const router = useRouter();
+
+  if (!isAuthenticated) {
+    router.push("/login");
+  }
+
   return <FeedbackForm />;
 };
 
